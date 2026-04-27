d2c05eb1225317f13b2855a83e920bce887863f4
mmesomasaint <mmesomasaint@gmail.com>
2026-04-27 01:51:43 +0100

    Fix: ccxt pkg

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type {NextConfig} from "next";
+
+const nextConfig: NextConfig = {
   // This is the key. It tells the bundler: 
   // "Do not look inside these folders, just treat them as external"
   serverComponentsExternalPackages: ['ccxt'],
-  
-  webpack: (config) => {
-    config.externals.push({
-      // This specifically tells Webpack to ignore the protobufjs path issue
-      'protobufjs/minimal': 'commonjs protobufjs/minimal',
-    });
-    return config;
+}
 
 export default nextConfig;
